10a9f143b229a86a2476375912f837b11c145455
remmi755 <90457721+remmi755@users.noreply.github.com>
2023-04-02 21:07:11 +0300

    add all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Block } from './Block';
+import './index.scss';
 
 function App() {
+  const [fromCurrency, setFromCurrency] = React.useState('UAH')
+  const [toCurrency, setToCurrency] = React.useState('USD')
+  const [fromPrice, setFromPrice] = React.useState('')
+  const [toPrice, setToPrice] = React.useState('')
+  const [rates, setRates] = React.useState([])
+  // const ratesRef = React.useRef([])
+
+  React.useEffect(() => {
+    fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+        .then((res) => res.json())
+        .then((json) => {
+          const date = json[0].exchangedate
+          json = [...json, {r030: 1, txt: 'Українська гривна', rate: 1, cc: 'UAH', exchangedate: date}]
+          setRates(json)
+          console.log(json)
+          // ratesRef.current = json;
+        })
+        .catch((err) => {
+          console.warn(err)
+          alert('Не удалось получить информацию')
+        })
+  }, [])
+
+  const foundCurrencyFrom = () => {
+    const currencyValueFrom = rates.filter((cur) => cur.cc === fromCurrency);
+    return  currencyValueFrom[0]?.rate;
+  }
+
+  const foundCurrencyTo = () => {
+    const  currencyValueTo = rates.filter((cur) => cur.cc === toCurrency);
+    return  currencyValueTo[0]?.rate;
+  }
+
+  let valueFrom = foundCurrencyFrom()
+  let valueTo = foundCurrencyTo()
+
+  const onChangeFromPrice =  (value) => {
+    const result =  (valueFrom / valueTo * value);
+    setToPrice(result.toFixed(2))
+    setFromPrice(value)
+  }
+
+  const onChangeToPrice =  (value) => {
+    const result = (valueTo / valueFrom) * value
+    setFromPrice(result.toFixed(2))
+    setToPrice(value)
+  }
+
+  React.useEffect(() => {
+    onChangeFromPrice(fromPrice)
+  }, [fromCurrency])
+
+  React.useEffect(() => {
+    onChangeToPrice(toPrice)
+  }, [toCurrency])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <div className="title">
+              <a href="https://git.io/typing-svg">
+                  <img src="https://readme-typing-svg.demolab.com?font=Fira+Code&weight=700&size=40&duration=4000&pause=1000&width=700&height=100&lines=Milena's+currency+converter;real+time"
+                                                       alt="Typing SVG" />
+              </a>
+          </div>
+          <div className="App">
+              <Block value={fromPrice}
+                     currency={fromCurrency}
+                     onChangeCurrency={setFromCurrency}
+                     onChangeValue={onChangeFromPrice}/>
+              <Block value={toPrice}
+                     currency={toCurrency}
+                     onChangeCurrency={setToCurrency}
+                     onChangeValue={onChangeToPrice}/>
+          </div>
+      </>
+
   );
 }
 
